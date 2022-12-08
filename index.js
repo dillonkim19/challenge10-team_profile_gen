e2de4c3f2433a524cd558a5a6803d287b9349ef5
Dillon Kim <dillonkim@Dillons-MacBook-Air.local>
2022-12-08 04:21:15 -0800

    add generateHTML functions for each role

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ const fs = require('fs');
 
 // initialize employees array
 employees = []
-managers = []
-engineers = []
-interns = []
 
 // questions to ask everyone and specific role
 generalQuestions = [
@@ -73,36 +70,118 @@ internQuestions = [
 
 const prompt = inquirer.createPromptModule();
 
+function managerCard(manager) {
+    return /*html*/`
+    <div class="col-sm-3">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">${manager.name}</h3>
+            </div>
+            <div class="card-body">
+                <h4 class="card-text">${manager.getRole()}</h4>
+                <p class="card-text">${manager.officeNumber}</p>
+                <p class="card-text">${manager.id}</p>
+                <p class="card-text">${manager.email}</p>
+            </div>
+        </div>
+    </div>
+    `
+}
+
+function engineerCard(engineer) {
+    return /*html*/ `
+    <div class="col-sm-3">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">${engineer.name}</h3>
+            </div>
+            <div class="card-body">
+                <h4 class="card-text">${engineer.getRole()}</h4>
+                <p class="card-text">${engineer.github}</p>
+                <p class="card-text">${engineer.id}</p>
+                <p class="card-text">${engineer.email}</p>
+            </div>
+        </div>
+    </div>
+    `
+}
+
+function internCard(intern) {
+    return /*html*/ `
+    <div class="col-sm-3">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">${intern.name}</h3>
+            </div>
+            <div class="card-body">
+                <h4 class="card-text">${intern.getRole()}</h4>
+                <p class="card-text">${intern.school}</p>
+                <p class="card-text">${intern.id}</p>
+                <p class="card-text">${intern.email}</p>
+            </div>
+        </div>
+    </div>
+    `
+}
+
+function generateHTML() {
+    return /*html*/`
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>TeamProfile</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1>Team Profile</h1>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row justify-content-center">
+                ${employees.map((employee) => {
+                    switch (employee.getRole()) {
+                        case "Manager":
+                            return managerCard(employee);
+                        case "Engineer":
+                            return engineerCard(employee);
+                        case "Intern":
+                            return internCard(employee);
+                    }
+                }).join("")}
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    </body>
+    </html>
+    `
+}
 // Note: .join('') after map function "removes" the commas that come after each array
 //       this is because the map function returns an array that'll separate each element by a comma
 function renderHTMLFile() {
     // figure out how to extract managers, engineers, interns into different arrays
     console.log(employees)
 
-    for (let i = 0; i < employees.length; i++) {
-        console.log(employees[i])
-        console.log(employees[i].officeNumber == !undefined)
-        console.log(employees[i].github == !undefined)
-    }
+    fs.writeFileSync('./dist/index.html', generateHTML())
 
 
-
-    fs.writeFileSync('./dist/previous.html', /*html*/`
-        <ul>
-            ${employees.map(employee => /*html*/`
-                <li>
-                    <h1>${employee.getName()}</h1>
-                    <h2>${employee.getRole()}</h2>
-                    <p>${employee.id}</p>
-                    <p>${employee.email}</p>
-                </li>
-            `).join('')}
-        </ul>
-    `)
-
-    fs.writeFileSync('./dist/index.html', /*html*/`
-        generateHTML(data)
-    `)
+    // fs.writeFileSync('./dist/previous.html', /*html*/`
+    //     <ul>
+    //         ${employees.map(employee => /*html*/`
+    //             <li>
+    //                 <h1>${employee.getName()}</h1>
+    //                 <h2>${employee.getRole()}</h2>
+    //                 <p>${employee.id}</p>
+    //                 <p>${employee.email}</p>
+    //             </li>
+    //         `).join('')}
+    //     </ul>
+    // `)
     // fs.appendFileSync('./dist/index.html', /*html*/`
     //     <ul>
     //         ${employees.map(employee => /*html*/`
